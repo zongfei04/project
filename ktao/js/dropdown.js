e82d5900230c0076e8410503c2b824db95af5bca
zongfei04 <2474954232@qq.com>
2019-09-26 11:56:27 +0800

    add

--- ktao/js/dropdown.js
+++ ktao/js/dropdown.js
@@ -5,6 +5,7 @@
 		this.options = options;
 		this.$layer = $elem.find('.dropdown-layer')
 		activeClass = $elem.data('active') + "-active";
+		this.Timer = 0;
 
 		//2初识化
 		this.init()
@@ -18,27 +19,62 @@
 			this.$layer.showHide(this.options);
 			//2.绑定显示隐藏事件
 
-			/*this.$elem.on('show shown hide hiden',function(ev){
+			this.$elem.on('show shown hide hiden',function(ev){
 				this.$elem.trigger('dropdown-'+ev.type);
-			}.bind(this))*/
+			}.bind(this))
 			//3绑定事件
-			this.$elem.hover($.proxy(this.show,this),$.proxy(this.hide,this));
+			if(this.options.eventName == 'click'){
+				this.$elem.on('click',function(ev){
+					ev.stopPropagation();
+					this.show();
+				}.bind(this));
+				$(document).on('click',$.proxy(this.hide,this))
+			}
+			else{
+				this.$elem.hover($.proxy(this.show,this),$.proxy(this.hide,this));
+			}
+			
 
 		},
 		show:function(){
-			this.$elem.addClass(activeClass);
-			this.$layer.showHide('show')
+			if(this.options.delay){
+				this.timer = setTimeout(function(){
+					this.$elem.addClass(activeClass);
+					this.$layer.showHide('show')
+				}.bind(this),this.options.delay)
+			}
+			else{
+				this.$elem.addClass(activeClass);
+				this.$layer.showHide('show')
+			}
+			
+
 		},
 		hide:function(){
+			clearTimeout(this.timer)
 			this.$elem.removeClass(activeClass);
 			this.$layer.showHide('hide')
 		}
+	}
+	Dropdown.DEFAULS = {
+		js:true,
+		mode:'slideDownUp',
+		delay:''
 	}
 	$.fn.extend({
 		dropdown:function(options){
 			return this.each(function(){
 				var $elem = $(this);
-				new Dropdown($elem,options);
+				var dropdown = $elem.data('dropdown');
+				if(!dropdown){
+					options = $elem.extend({},Dropdown.DEFAULS,options)
+					dropdown = new Dropdown($elem,options);
+					$elem.data('dropdown',dropdown);
+				}
+				if(typeof dropdown[options] == 'function'){
+					dropdown[options]();
+				}
+				
 			})
 		}
 	})
--- ktao/js/index.js
+++ ktao/js/index.js
@@ -1,7 +1,11 @@
 // 我的跨淘下拉列表的实现
 ;(function($){
-	$('.dropdown').dropdown({js:true,mode:"slideDownUp"});
-	/*$('.dropdown').on('dropdown-show dropdown-shown dropdown-hide dropdown-hiden',function(ev){
-
-	})*/
+	$('.dropdown').dropdown({js:true,mode:"slideDownUp",delay:200,eventName:'click'});
+	$('.dropdown').on('dropdown-show dropdown-shown dropdown-hide dropdown-hiden',function(ev){
+		console.log(ev.type)
+	})
+	$('button').on('click',function(ev){
+		ev.stopPropagation();
+		$('.nav-side .dropdown').dropdown('show')
+	})
 })(jQuery);
