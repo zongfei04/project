af6a35fa20b21b468b9c1492faa5665215d30cc5
zongfei04 <2474954232@qq.com>
2019-10-14 17:08:01 +0800

    add

--- ktao/js/carousel.js
+++ ktao/js/carousel.js
@@ -5,8 +5,10 @@
 		this.options = options;
 		this.now = this.options.showindex;
 		this.$carouselItem = this.$elem.find('.carousel-item');
+		this.itemNum = this.$carouselItem.length;
 		this.$btnItem = this.$elem.find('.btn-item');
 		this.$control = this.$elem.find('.control');
+		this.timer = 0;
 		
 
 		//2初识化
@@ -26,7 +28,7 @@
 				this.$carouselItem.eq(this.now).show();
 				this.$btnItem.eq(this.now).addClass('active');
 				//将显示隐藏初始化
-				this.$carouselItem.showHide({js:true,mode:"fade"});
+				this.$carouselItem.showHide();
 				//将左右按钮显示(监听事件)
 				this.$elem.hover(function(){
 					this.$control.show();
@@ -34,16 +36,29 @@
 					this.$control.hide();
 				}.bind(this))
 				.on('click','.control-left',function(){
-					this._fade(this.now - 1);
+					this._fade(this.getIndex(this.now - 1));
 				}.bind(this))
 				.on('click','.control-right',function(){
-					this._fade(this.now + 1);
+					this._fade(this.getIndex(this.now + 1));
 
-				}.bind(this))
+				}.bind(this));
+				//处理自动轮播
+				if(this.options.interval){
+					this.autoplay();
+					this.$elem.hover($.proxy(this.paused,this),$.proxy(this.autoplay,this))
+				}
+				//处理底部按钮的点击事件
+				var _this = this;
+				this.$btnItem.on('click',function(){
+					
+					_this._fade(_this.$btnItem.index($(this)))
+				})
 				
 			}
 		},
 		_fade:function(index){
+			//如果当前的值和即将显示的值相同不显示
+			if(this.now == index) return;
 			//将当前显示的隐藏
 			this.$carouselItem.eq(this.now).showHide('hide');
 			this.$btnItem.eq(this.now).removeClass('active');
@@ -53,12 +68,32 @@
 			//把index的值赋给this.now
 			this.now = index;
 
+
+		},
+		getIndex:function(index){
+			if(index<0) return this.itemNum-1;
+			if(index>=this.itemNum) return 0;
+
+			return index; 
+		},
+		// 自动轮播
+		autoplay:function(){
+
+			this.timer = setInterval(function(){
+				this.$control.eq(1).trigger('click');
+			}.bind(this),this.options.interval)
+		},
+		//停止轮播
+		paused:function(){
+			clearInterval(this.timer)
 		}
-		
 	}
 	Carousel.DEFAULS = {
 		slide:false,
-		showindex:0
+		showindex:0,
+		interval:1000,
+		js:true,
+		mode:"fade"
 	}
 	$.fn.extend({
 		carousel:function(options){
